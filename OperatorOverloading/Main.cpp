ff37b9275e968b3c6b7bbf54fcbb089d943757b7
rapidswap <curudow7988@gmail.com>
2026-07-31 10:01:37 +0900

    operator

---
+++ OperatorOverloading/Main.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+#include"Point.h"
+
+// 출력 연산자 오버로딩.
+std::ostream& operator<<(std::ostream& os, const Point& point)
+{
+	std::cout << "(" << point.x << ", " << point.y << ")\n";
+	return os;
+}
+
+int main()
+{
+	// 포인터 객체 간의 덧셈 연산.
+	Point p1(1, 1);
+	Point p2(2, 2);
+
+	Point p3 = p1 + p2;
+	//Point p3 = p1.operator+(p2);
+
+	Point p4 = p1 - p2;
+	//Point p4 = operator-(p1,p2);
+
+
+	// 결과 값 출력.
+	std::cout << p3 << p4;
+
+}
---
+++ OperatorOverloading/Point.h
@@ -0,0 +1,25 @@
+﻿#pragma once		// 인클루드 가드
+#include<iostream>
+
+
+class Point
+{
+public:
+	Point(int x, int y);
+
+	// 덧셈 연산자 오버로딩.
+	Point operator+(const Point& other) const;
+
+	// friend로 연산자 오버로딩
+	friend Point operator-(const Point& left, const Point& right);
+
+	friend std::ostream& operator<<(std::ostream& os, const Point& point);
+
+	
+	void Print() const;
+
+private:
+	int x = 0;
+	int y = 0;
+};
+
