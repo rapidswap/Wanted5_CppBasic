fbf9dcc56a298ffbd649945306fd5e3f6ae2ee2a
Hyeongjin Kang <73596570+rapidswap@users.noreply.github.com>
2026-08-06 11:28:02 +0900

    t

---
+++ SharedPtr/Main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <memory>
+
+class Actor
+{
+public:
+	virtual ~Actor()=default;
+};
+
+class Player : public Actor
+{
+
+};
+
+int main()
+{
+	std::shared_ptr<Actor> actor = std::make_shared<Actor>();
+	std::shared_ptr<Player>player = std::make_shared<Player>();
+
+	// shared_ptr은 소유권 공유 가능.
+	// 참조(사용) 회수를 추적.
+	std::shared_ptr<Player> otherPlayer = player;
+	
+}
--- UniquePtr/Main.cpp
+++ UniquePtr/Main.cpp
@@ -20,11 +20,28 @@ public:
 		std::cout << "Player destroyed.\n";
 	}
 
-	void Attack()
+	void Attack() const
 	{
 		std::cout << "Player attacks.\n";
 	}
 };
+
+// 함수로 전달 받기.
+void AttackPlayer(Player* player)
+{
+	player->Attack();
+	delete player;
+}
+
+void AttackPlayer2(const Player& player)
+{
+	player.Attack();
+}
+
+void AttackPlayer3(const std::unique_ptr<Player>& player)
+{
+	player->Attack();
+}
 
 int main()
 {
@@ -42,4 +59,31 @@ int main()
 
 	// 레퍼런스(역참조) 방법도 기존 포인터와 동일.
 	Player& playerRef = *player;
+	
+	// 다른 스마트 포인터.
+	// unique_ptr은 소유권 공유가 안됨.
+	// std::unique_ptr<Player> player2 = player;
+
+	// 주소를 빌려서 사용하는 방법.
+	// 1. RawPointer(원시 포인터)로 받아서 사용.
+	// 위험! -> 실수로 delete를 할 수 있음.
+	//Player* player2 = player.get();
+	//delete player2;
+	AttackPlayer(player.get());
+
+	// 2. 레퍼런스로 전달 받기.
+	// 원시 포인터보다 조금 더 안전한 방법.
+	AttackPlayer2(*player);
+
+	// 3. 스마트 포인터의 레퍼런스를 사용.
+	// 3번 방법도 가능은 하나 unique_ptr 정의/사용 방식에 위배.
+	AttackPlayer3(player);
+
+	// unique_ptr의 소유권을 다른 곳으로 넘기는 방법.
+	// ValueCategory와 연결
+	// 이전을 다른 곳으로 한 이후로는 기존에 사용한 player를 사용하면 안됨.
+	std::unique_ptr<Player> otherPlayer = std::move(player);
+	// 이전을 한 뒤로는 기존 포인터를 리셋(초기화) 해주는게 좋음.
+	player.reset();
+
 }
